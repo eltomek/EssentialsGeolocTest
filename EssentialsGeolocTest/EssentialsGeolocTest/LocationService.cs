0a10103dd64ad2c5848a851dd7f76fb4eb5505f5
eltomek <tlorek@gmail.com>
2021-03-09 23:12:59 +0100

    Add project files.

---
+++ EssentialsGeolocTest/EssentialsGeolocTest/LocationService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace EssentialsGeolocTest
+{
+    public static class LocationService
+    {
+        public static async Task<Location> GetLastKnownLocation()
+        {
+            Location lastKnownLocation = null;
+            try
+            {
+                lastKnownLocation = await Geolocation.GetLastKnownLocationAsync();
+
+                if (lastKnownLocation != null)
+                {
+                    Debug.WriteLine($"Last known location: {lastKnownLocation?.ToString()}");
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }
+            return lastKnownLocation;
+        }
+
+        public static async Task<Location> GetCurrentLocation()
+        {
+            Location actualLocation = null;
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Best, TimeSpan.FromSeconds(10));
+                actualLocation = await Geolocation.GetLocationAsync(request);
+
+                if (actualLocation != null)
+                {
+                    Debug.WriteLine($"Actual location: {actualLocation?.ToString()}");
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }
+            return actualLocation;
+        }
+    }
+}
---
+++ EssentialsGeolocTest/EssentialsGeolocTest/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using System.Diagnostics;
+
+namespace EssentialsGeolocTest
+{
+    public partial class MainPage : ContentPage
+    {
+        private static System.Timers.Timer locationUpdateTimer;
+        private Location currentLocation;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            locationUpdateTimer = new System.Timers.Timer(1000) { AutoReset = true };
+
+            locationUpdateTimer.Elapsed += async (o, e) =>
+            {
+                currentLocation = await LocationService.GetCurrentLocation();
+                Debug.WriteLine($"Curr. location: {currentLocation?.ToString()}");
+            };
+            
+            locationUpdateTimer.Start();
+
+        }
+
+        protected override async void OnAppearing()
+        {
+
+            Location currentLocation;
+            
+            if ((currentLocation = await LocationService.GetCurrentLocation()) != null)
+            {
+                Debug.WriteLine($"OnAppearing currentLocation: {currentLocation?.ToString()}");
+            }
+
+            base.OnAppearing();
+
+        }
+    }
+}
